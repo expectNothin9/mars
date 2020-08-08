74814b76e3f725934f6867ad0545f2010fa9903a
expectNothin9 <expect.nothin9@gmail.com>
2020-08-08 16:05:28 +0800

    prop-types and fix eslint error

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { useApollo } from "../apollo/client";
 
 import "../styles/global.css";
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
     </ApolloProvider>
   );
-}
+};
+App.propTypes = {
+  Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
+  pageProps: PropTypes.object,
+};
+
+export default App;
